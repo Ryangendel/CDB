6e7ea133fd4b6f43dff0a9ca3f097e0e4c3f2ed7
Ryan Gendel <ryangendel@Ryans-MacBook-Pro.local>
2025-02-25 17:31:06 -0700

    built server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require("path")
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname,'./public/homepage.html'));
+})
+
+// app.get("/comingsoon", (req, res)=>{
+//     res.sendFile(path.join(__dirname,'./public/pages/index.html'));
+// })
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!!!!!`);
+    console.log(`App running on port ${PORT}!!!!!`);
+});
